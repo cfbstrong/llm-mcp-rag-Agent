d9e40960672b2b5fc821c1f6ce0438cf7cbd95d6
fengbin_chen <1563540102@qq.com>
2026-01-07 22:41:48 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import { hello } from "./utils";
+import MCPClient from "./MCPClient";
+import Agent from "./Agent";
 
-hello();
+const currentDir = process.cwd(); //当前工作文件夹
+
+const fetchMcp = new MCPClient("fetch", "uvx", ["mcp-server-fetch"]);
+const fileMcp = new MCPClient("file", "npx", [
+  "-y",
+  "@modelcontextprotocol/server-filesystem",
+  currentDir,
+]);
+
+async function main() {
+  const agent = new Agent("gpt-4o-mini", [fetchMcp, fileMcp]);
+  await agent.init();
+  const response = await agent.invoke(
+    `爬取https://news.ycombinator.com/的内容,并且总结后保存到${currentDir}的news.md文件中`
+  );
+  console.log(response);
+}
+
+main();
