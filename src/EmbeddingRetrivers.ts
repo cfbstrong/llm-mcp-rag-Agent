08e2c0a01a54953fc01a4e54fd2589279fcbfae5
fengbin_chen <1563540102@qq.com>
2026-01-08 13:29:39 +0800

    feature: Calculate the cosine similarity

--- src/EmbeddingRetrivers.ts
+++ src/EmbeddingRetrivers.ts
@@ -9,7 +9,21 @@ export default class EmbeddingRetrivers {
     this.vectorStore = new VectorStore();
   }
 
-  async embed(document: string): Promise<number[]> {
+  async embedQuery(query: string): Promise<number[]> {
+    //将prompt转换为向量
+    return this.embed(query);
+  }
+
+  async embedDocument(document: string): Promise<number[]> {
+    //将私域知识转换为向量
+    this.vectorStore.add({
+      embeddings: await this.embed(document),
+      document,
+    });
+    return this.embed(document);
+  }
+
+  private async embed(document: string): Promise<number[]> {
     //将文本转换为向量
     const response = await fetch(
       `${process.env.EMBEDDING_BASE_URL}/embedding`,
@@ -27,5 +41,10 @@ export default class EmbeddingRetrivers {
     );
     const data = await response.json();
     return data.data[0].embedding;
+  }
+
+  async retrieve(query: string, k: number) {
+    const queryEmbedding = await this.embedQuery(query);
+    return this.vectorStore.search(queryEmbedding, k);
   }
 }
--- src/VectorStore.ts
+++ src/VectorStore.ts
@@ -14,5 +14,32 @@ export default class VectorStore {
     this.VectorStore.push(item); // 将项目添加到items数组的末尾
   }
 
-  search(query: number[], k: number): VectorStoreItem[] {}
+  search(queryEmbeddings: number[], k: number): VectorStoreItem[] {
+    const scored = this.VectorStore.map((item) => {
+      const score = this._calculateCosineSimilarity(
+        queryEmbeddings,
+        item.embeddings
+      );
+      return { ...item, score };
+    });
+    return scored.sort((a, b) => b.score - a.score).slice(0, k);
+  }
+
+  private _calculateCosineSimilarity(
+    embedding1: number[],
+    embedding2: number[]
+  ): number {
+    // 计算余弦相似度
+    const dotProduct = embedding1.reduce(
+      (sum, value, index) => sum + value * embedding2[index],
+      0
+    );
+    const magnitude1 = Math.sqrt(
+      embedding1.reduce((sum, value) => sum + value * value, 0)
+    );
+    const magnitude2 = Math.sqrt(
+      embedding2.reduce((sum, value) => sum + value * value, 0)
+    );
+    return dotProduct / (magnitude1 * magnitude2);
+  }
 }
